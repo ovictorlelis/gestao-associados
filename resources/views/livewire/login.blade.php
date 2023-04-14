e9ebaf43343a25fb9f4c17f9f83a6645f2b06be4
Victor Lelis <ovictorlelis@gmail.com>
2023-04-14 10:31:10 -0300

    add bootstrap

--- resources/views/livewire/login.blade.php
+++ resources/views/livewire/login.blade.php
@@ -1,12 +1,24 @@
 <div>
-    <h1>Login</h1>
-
-    {{ $user }}
-    <form wire:submit.prevent="test">
-        <input class="border" id="user" name="user" type="text" wire:model="user">
-        <input type="submit" value="Enviar">
-        @error('user')
-            {{ $message }}
-        @enderror
-    </form>
+    <section class="text-gray-600 body-font">
+        <div class="container px-5 py-24 mx-auto flex flex-wrap items-center">
+            <div class="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
+                <h2 class="text-gray-900 text-lg font-medium title-font mb-5">Entrar</h2>
+                <div class="relative mb-4">
+                    <label class="leading-7 text-sm text-gray-600" for="full-name">Usuário</label>
+                    <input
+                        class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                        id="full-name" name="full-name" type="text">
+                </div>
+                <div class="relative mb-4">
+                    <label class="leading-7 text-sm text-gray-600" for="email">Senha</label>
+                    <input
+                        class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                        id="email" name="email" type="email">
+                </div>
+                <button
+                    class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+                <p class="text-xs text-gray-500 mt-3">Literally you probably haven't heard of them jean shorts.</p>
+            </div>
+        </div>
+    </section>
 </div>
